9fd210a23c67c918cbad85f269b863bda6398990
anordy <anordypatrick98@gmail.com>
2022-06-24 00:52:01 +0300

    welcome screen

--- lib/app/retailer/pages/auth/welcome_page.dart
+++ lib/app/retailer/pages/auth/welcome_page.dart
@@ -8,7 +8,6 @@ import 'package:uza_sales/app/retailer/widget/utils.dart';
 
 import '../../../orderBooker/pages/booker_create_shop.dart';
 
-
 class WelcomePage extends StatefulWidget {
   _WelcomePage createState() => _WelcomePage();
 }
@@ -22,114 +21,109 @@ class _WelcomePage extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.bgScreen,
-      body: GestureDetector(
-          onTap: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
-        child: Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.white),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                     Container(
-                      // color: Colors.green,
-                      height: 110,
-                      width: 200,
-                      child:
-                          Image(image: AssetImage("assets/icons/v1.png")),
-                    ),
-                    ],
+        backgroundColor: AppColor.bgScreen,
+        body: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+            },
+            child: Scaffold(
+              body: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(color: Colors.white),
                   ),
-                ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                // color: Colors.green,
+                                height: 110,
+                                width: 200,
+                                child: Image(
+                                    image: AssetImage("assets/icons/v1.png")),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CustomButton(
+                              color: AppColor.base,
+                              height: 60,
+                              width: Utils.displayWidth(context) * 0.65,
+                              inputText: Text(
+                                AppLocalizations.of(context)
+                                    .login
+                                    .toUpperCase(),
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LoginPage()));
+                                // Navigator.push(context,
+                                //     MaterialPageRoute(builder: (context) => SignupPage()));
+                              },
+                            ),
+                            // SizedBox(
+                            //   height: 10,
+                            // ),
+                            //   Text(AppLocalizations.of(context).or,style: TextStyle(color: Color(0xFF40403F),fontSize: 14),),
+                            //    SizedBox(
+                            //     height: 10,
+                            //   ),
+                            //  Container(
+                            //    decoration: BoxDecoration(
+                            //      borderRadius: BorderRadius.circular(60),
+                            //      border: Border.all(color: AppColor.base)
+                            //    ),
+                            //    child: MaterialButton(
+                            //       shape: RoundedRectangleBorder(
+                            //         borderRadius: BorderRadius.circular(60.0)),
+                            //     color: Colors.white,
+                            //     height: 58,
+                            //     minWidth: Utils.displayWidth(context) * 0.65,
+                            //     child: Text(AppLocalizations.of(context).login.toUpperCase(),style: TextStyle(color: Color(0xFF40403F),fontSize: 16),),
+                            //      onPressed: () {
+                            //        Navigator.push(context,
+                            //           MaterialPageRoute(builder: (context) => LoginPage()));
+                            //      }
+                            //    ),
+                            //  ),
+                            //    SizedBox(
+                            //     height: 10,
+                            //   ),
+                            //   TextButton(onPressed: () {
+                            //      Navigator.pushReplacement(context,
+                            //           MaterialPageRoute(builder: (context) => HomePage()));
+                            //   }, child: Text(AppLocalizations.of(context).skip.toUpperCase(),style: TextStyle(color: Color(0xFF40403F),fontSize: 16),)),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
-              //   CustomButton(
-              //   color: AppColor.base,
-              //   height: 60,
-              //   width: Utils.displayWidth(context) * 0.65,
-              //   inputText: Text(
-              //   "Sales".toUpperCase(),
-              //   style: TextStyle(color: Colors.white, fontSize: 16),
-              // ),
-              //   onPressed: () {
-              //     Navigator.push(context,
-              //         MaterialPageRoute(builder: (context) => SalesHome()));
-              //   },
-              // ),
-              SizedBox(height: 10,),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CustomButton(
-                color: AppColor.base,
-                height: 60,
-                width: Utils.displayWidth(context) * 0.65,
-                inputText: Text(
-                AppLocalizations.of(context).create_shop.toUpperCase(),
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-                onPressed: () {
-                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => BookerCreateShop()));
-                  // Navigator.push(context,
-                  //     MaterialPageRoute(builder: (context) => SignupPage()));
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(AppLocalizations.of(context).or,style: TextStyle(color: Color(0xFF40403F),fontSize: 14),),
-               SizedBox(
-                height: 10,
-              ),
-             Container(
-               decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(60),
-                 border: Border.all(color: AppColor.base)
-               ),
-               child: MaterialButton(
-                  shape: RoundedRectangleBorder(   
-                    borderRadius: BorderRadius.circular(60.0)),
-                color: Colors.white,
-                height: 58,
-                minWidth: Utils.displayWidth(context) * 0.65,
-                child: Text(AppLocalizations.of(context).login.toUpperCase(),style: TextStyle(color: Color(0xFF40403F),fontSize: 16),),
-                 onPressed: () {
-                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginPage()));
-                 }
-               ),
-             ),
-               SizedBox(
-                height: 10,
-              ),
-              TextButton(onPressed: () {
-                 Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => HomePage()));
-              }, child: Text(AppLocalizations.of(context).skip.toUpperCase(),style: TextStyle(color: Color(0xFF40403F),fontSize: 16),)),
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    ))
+            ))
 
-      // ),
-    );
+        // ),
+        );
   }
 }
